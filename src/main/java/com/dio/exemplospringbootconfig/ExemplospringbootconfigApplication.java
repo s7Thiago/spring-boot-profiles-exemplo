e18d0f79a511edd875a9151ea28ab53892e1cd65
Thiago Silva <thyagosousasilva@gmail.com>
2021-10-01 13:50:45 -0300

    Criando projeto com arquivos .priperties para os environments dev e prod

---
+++ src/main/java/com/dio/exemplospringbootconfig/ExemplospringbootconfigApplication.java
@@ -0,0 +1,13 @@
+package com.dio.exemplospringbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplospringbootconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplospringbootconfigApplication.class, args);
+	}
+
+}
